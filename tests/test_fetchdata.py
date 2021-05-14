9e6653744f105ad727e11269fcdfaf83a0106aaf
kyoya <m.kyoya777@gmail.com>
2021-05-14 19:15:01 +0900

    test method that scrape_details

--- tests/test_fetchdata.py
+++ tests/test_fetchdata.py
@@ -4,8 +4,11 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import pytest
 import requests
 
-from src.croning_naist_syllabus.FetchData import (FetchData, LectureDetail,
-                                                  LectureNameUrl)
+from src.croning_naist_syllabus.FetchData import (
+    FetchData,
+    LectureDetail,
+    LectureNameUrl,
+)
 
 
 @pytest.fixture(scope="session")
@@ -69,24 +72,24 @@ specialized_lecture = LectureNameUrl(
         (FetchData.LECTURE_TYPE_SPECIALIZED, specialized_lecture),
     ],
 )
-def test_get_name_and_url_with_basic(
+def test_scrape_name_and_url_with_basic(
     start_http_server, fetch_data, lecture_type, contained_data
 ):
-    name_and_url_list = fetch_data.get_name_and_url(lecture_type)
+    name_and_url_list = fetch_data.scrape_name_and_url(lecture_type)
     assert contained_data in name_and_url_list
 
 
-def test_get_name_and_url_key_error(start_http_server, fetch_data):
+def test_scrape_name_and_url_key_error(start_http_server, fetch_data):
     with pytest.raises(KeyError):
-        fetch_data.get_name_and_url("key error")
+        fetch_data.scrape_name_and_url("key error")
 
 
 def dummy_init(self, url):
     pass
 
 
-def test_choose_lecture(start_http_server, monkeypatch):
-    def dummy_get_name_and_url(self, lecture):
+def test_scrape_lecture(start_http_server, monkeypatch):
+    def dummy_scrape_name_and_url(self, lecture):
         return [
             LectureNameUrl(
                 name="例",
@@ -95,10 +98,10 @@ def test_choose_lecture(start_http_server, monkeypatch):
         ]
 
     monkeypatch.setattr(FetchData, "__init__", dummy_init)
-    monkeypatch.setattr(FetchData, "get_name_and_url", dummy_get_name_and_url)
+    monkeypatch.setattr(FetchData, "scrape_name_and_url", dummy_scrape_name_and_url)
     fetch_data = FetchData("url")
 
-    fetch_data.choose_lecture([FetchData.LECTURE_TYPE_BASIC])
+    fetch_data.scrape_lectures([FetchData.LECTURE_TYPE_BASIC])
 
     assert (
         LectureNameUrl(name="例", url="http://example.com")
@@ -106,13 +109,63 @@ def test_choose_lecture(start_http_server, monkeypatch):
     )
 
 
-def test_get_detail_of_lecture(start_http_server, monkeypatch):
+def test_scrape_detail_of_lecture(start_http_server, monkeypatch):
 
     monkeypatch.setattr(FetchData, "__init__", dummy_init)
     fetch_data = FetchData("url")
     detail_url = "http://127.0.0.1:8888/tests/detail_1.html"
     response: requests.Response = requests.get(detail_url)
-    detail_lecture_data = fetch_data.get_detail_of_lecture(response)
+    detail_lecture_data = fetch_data.scrape_detail_of_lecture(response)
     assert 1 == detail_lecture_data[0].number
     assert "4/22 [2]" == detail_lecture_data[0].date
     assert "スーパスカラとVLIW (日本語教科書８章)" == detail_lecture_data[0].theme
+
+
+def test_scrape_details(start_http_server, monkeypatch):
+
+    lecture_detail = LectureDetail(
+        number=1,
+        date="4/22 [2]",
+        theme="スーパスカラとVLIW (日本語教科書８章)",
+        content="高性能基盤の説明です",
+    )
+
+    monkeypatch.setattr(FetchData, "__init__", dummy_init)
+    monkeypatch.setattr(
+        FetchData,
+        "scrape_detail_of_lecture",
+        lambda self, response: [lecture_detail],
+    )
+
+    detail_url = "http://127.0.0.1:8888/tests/detail_1.html"
+    lecture = LectureNameUrl(name="高性能計算基盤", url=detail_url)
+    fetch_data = FetchData("url")
+    fetch_data.scrape_details([lecture])
+
+    assert lecture_detail in fetch_data.lectures_details["高性能計算基盤"]
+
+
+@pytest.mark.parametrize(
+    "invalid_url",
+    [
+        "http://127.0.0.1:8888/not_existed_index.html",
+        "httpaaaa",
+    ],
+)
+def test_scrape_details_with_invalid_url(start_http_server, monkeypatch, invalid_url):
+
+    monkeypatch.setattr(FetchData, "__init__", dummy_init)
+    lecture = LectureNameUrl(name="高性能計算基盤", url=invalid_url)
+    fetch_data = FetchData("url")
+    with pytest.raises(Exception):
+        fetch_data.scrape_details([lecture])
+
+
+def test_scrape_details_with_not_lecture_name_url(start_http_server, monkeypatch):
+    monkeypatch.setattr(FetchData, "__init__", dummy_init)
+    detail_url = "http://127.0.0.1:8888/tests/detail_1.html"
+    not_instance_of_lecture_name_url = {"name": "高性能計算基盤", "url": detail_url}
+    fetch_data = FetchData("url")
+    fetch_data.scrape_details(not_instance_of_lecture_name_url)
+
+    assert fetch_data.lectures_details == {}
