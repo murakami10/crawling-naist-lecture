41c0183a0a7b0fbf8ed7c89652da62d8f64472b0
kyoya <m.kyoya777@gmail.com>
2021-05-14 15:01:28 +0900

    make function which fetch data of lecture

--- src/croning_naist_syllabus/FetchData.py
+++ src/croning_naist_syllabus/FetchData.py
@@ -1,12 +1,69 @@
+from typing import NamedTuple
+
 import lxml.html
 import requests
 
 
 class FetchData:
+
+    LECTURE_TYPE_GENERAL = "general"
+    LECTURE_TYPE_INTRODUCTION = "introduction"
+    LECTURE_TYPE_BASIC = "basic"
+    LECTURE_TYPE_SPECIALIZED = "specialized"
+
+    start_index_of_lecture = {
+        LECTURE_TYPE_GENERAL: 5,
+        LECTURE_TYPE_INTRODUCTION: 35,
+        LECTURE_TYPE_BASIC: 40,
+        LECTURE_TYPE_SPECIALIZED: 67,
+    }
+
     def __init__(self, url: str):
         self.response: requests.Response = requests.get(url)
         # ステータスコードが200番以外なら例外を起こす
         self.response.raise_for_status()
+        self.root = lxml.html.fromstring(self.response.content)
 
-    def serach_for_lecture_name(self):
-        root = lxml.html.fromstring(self.response)
+    def choose_lecture(self, lectures: list):
+
+        self.name_and_url_of_lectures = {}
+
+        for lecture in lectures:
+
+            self.name_and_url_of_lectures[lecture] = self.get_name_and_url(lecture)
+
+    def get_name_and_url(self, lecture):
+        """
+        授業科目の名前とURLを取得
+        """
+        lecture_datas = []
+
+        # start_index_of_lectureにないkeyを指定すると例外を投げる
+        # 例外に気づくためにcatchしない
+        index = self.start_index_of_lecture[lecture]
+
+        while True:
+
+            value = self.root.cssselect(
+                f"#contents > table > tbody > tr:nth-child({index}) > td.w20pr"
+            )
+
+            if not value:
+                break
+
+            # aタグをのURLを調べ、Noneであれば"not exist"を代入
+            url = value[0].find("a")
+            if url is None:
+                url = "not exist"
+            else:
+                url = url.get("href")
+
+            lecture_datas.append(LectureNameUrl(name=value[0].text_content(), url=url))
+            index += 1
+
+        return lecture_datas
+
+
+class LectureNameUrl(NamedTuple):
+    name: str
+    url: str
--- tests/test_fetchdata.py
+++ tests/test_fetchdata.py
@@ -3,7 +3,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 import pytest
 
-from src.croning_naist_syllabus.FetchData import FetchData
+from src.croning_naist_syllabus.FetchData import FetchData, LectureNameUrl
 
 
 @pytest.fixture(scope="session")
@@ -16,6 +16,15 @@ def start_http_server():
     yield url
     server.shutdown()
     thred.join()
+
+
+@pytest.fixture()
+def fetch_data():
+    """
+    FetchDataのインスタンスを返す
+    """
+    fd = FetchData("http://127.0.0.1:8888/tests/syllabus.html")
+    return fd
 
 
 @pytest.mark.parametrize(
@@ -33,3 +42,62 @@ def test_init_with_invalid_url(start_http_server, invalid_url):
 def test_init_with_valid_url(start_http_server):
     valid_url = "http://127.0.0.1:8888/tests/index.html"
     _ = FetchData(valid_url)
+
+
+general_lecture = LectureNameUrl(
+    name="技術と倫理", url="https://syllabus.naist.jp/subjects/preview_detail/644"
+)
+introduction_lecture = LectureNameUrl(
+    name="情報理工学序論", url="https://syllabus.naist.jp/subjects/preview_detail/662"
+)
+basic_lecture = LectureNameUrl(
+    name="情報科学基礎Ⅰ", url="https://syllabus.naist.jp/subjects/preview_detail/791"
+)
+specialized_lecture = LectureNameUrl(
+    name="ソフトウェア工学", url="https://syllabus.naist.jp/subjects/preview_detail/688"
+)
+
+
+@pytest.mark.parametrize(
+    "lecture_type, contained_data",
+    [
+        (FetchData.LECTURE_TYPE_GENERAL, general_lecture),
+        (FetchData.LECTURE_TYPE_INTRODUCTION, introduction_lecture),
+        (FetchData.LECTURE_TYPE_BASIC, basic_lecture),
+        (FetchData.LECTURE_TYPE_SPECIALIZED, specialized_lecture),
+    ],
+)
+def test_get_name_and_url_with_basic(
+    start_http_server, fetch_data, lecture_type, contained_data
+):
+    name_and_url_list = fetch_data.get_name_and_url(lecture_type)
+    assert contained_data in name_and_url_list
+
+
+def test_get_name_and_url_key_error(start_http_server, fetch_data):
+    with pytest.raises(KeyError):
+        fetch_data.get_name_and_url("key error")
+
+
+def test_choose_lecture(start_http_server, monkeypatch):
+    def dummy_init(self, url):
+        pass
+
+    def dummy_get_name_and_url(self, lecture):
+        return [
+            LectureNameUrl(
+                name="例",
+                url="http://example.com",
+            )
+        ]
+
+    monkeypatch.setattr(FetchData, "__init__", dummy_init)
+    monkeypatch.setattr(FetchData, "get_name_and_url", dummy_get_name_and_url)
+    fetch_data = FetchData("url")
+
+    fetch_data.choose_lecture([FetchData.LECTURE_TYPE_BASIC])
+
+    assert (
+        LectureNameUrl(name="例", url="http://example.com")
+        in fetch_data.name_and_url_of_lectures[FetchData.LECTURE_TYPE_BASIC]
+    )
